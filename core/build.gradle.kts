eec834143f2996bdbd0c012acf10efc6ddaf5e12
strmt7 <28989754+strmt7@users.noreply.github.com>
2026-04-05 22:23:59 +0300

    Fix Android startup crash on physical devices

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -8,5 +8,4 @@ kotlin {
 
 dependencies {
     implementation("com.badlogicgames.gdx:gdx:1.13.5")
-    implementation("com.badlogicgames.gdx:gdx-bullet:1.13.5")
 }
--- core/src/main/kotlin/com/airdefense/game/AirDefenseGame.kt
+++ core/src/main/kotlin/com/airdefense/game/AirDefenseGame.kt
@@ -1,11 +1,9 @@
 package com.airdefense.game
 
 import com.badlogic.gdx.Game
-import com.badlogic.gdx.physics.bullet.Bullet
 
 class AirDefenseGame : Game() {
     override fun create() {
-        Bullet.init()
         setScreen(StartScreen(this))
     }
 }
