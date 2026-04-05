6e29ef13ec934b0c8fc3ff94f5839511b15d0243
strmt7 <28989754+strmt7@users.noreply.github.com>
2026-04-05 09:55:38 +0300

    Build Android 3D air defense prototype with physics

---
+++ android/proguard-rules.pro
@@ -0,0 +1,1 @@
+# Prototype build keeps default rules only.
---
+++ core/src/main/kotlin/com/airdefense/game/AirDefenseGame.kt
@@ -0,0 +1,9 @@
+package com.airdefense.game
+
+import com.badlogic.gdx.Game
+
+class AirDefenseGame : Game() {
+    override fun create() {
+        setScreen(BattleScreen())
+    }
+}
